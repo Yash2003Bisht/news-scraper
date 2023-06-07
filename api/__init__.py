e4ff48653a4d88701e0a63d303c01b06f5f8f6e7
yash2003bisht <yash2003bisht@gmail.com>
2023-06-07 18:11:45 +0530

    sentry integration done

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,31 @@
-from flask import Flask, json
+import os
 import logging
+from os.path import join, dirname
 
-app = Flask(__name__)
+import sentry_sdk
+from flask import Flask, json
+from sentry_sdk.integrations.flask import FlaskIntegration
+from dotenv import load_dotenv
+
+
+# ------------- load env -------------
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
+# -----------------------------------------
+
+
+# ------------- sentry integration -------------
+if os.environ.get("PRODUCTION", "false") != "false":
+    sentry_sdk.init(
+        dsn=os.environ["DSN"],
+        integrations=[
+            FlaskIntegration(),
+        ],
+        traces_sample_rate=1.0,
+        release="news-scraper@v1.3.1"
+    )
+# -----------------------------------------
+
 
 # ------------- logger config -------------
 logger = logging.getLogger(__name__)
@@ -18,11 +42,14 @@ logger.addHandler(ch)
 # -----------------------------------------
 
 
+app = Flask(__name__)
+
+
 def configure_app():
     from .endpoints import routes
 
     # add a secret key
-    app.config["SECRET_KEY"] = "5fsyuadigohkfg\g54d1fsggrteG45wtref4ad567f89gohp["
+    app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 
     # register blueprint
     app.register_blueprint(routes, url_prefix="/")
