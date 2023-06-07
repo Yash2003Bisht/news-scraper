ed746fcc90fb5fffba89f756bcf58a97e9581025
yash2003bisht <yash2003bisht@gmail.com>
2023-06-07 20:01:37 +0530

    version updated to v1.4.0

--- api/__init__.py
+++ api/__init__.py
@@ -22,7 +22,7 @@ if os.environ.get("PRODUCTION", "false") != "false":
             FlaskIntegration(),
         ],
         traces_sample_rate=1.0,
-        release="news-scraper@v1.3.1"
+        release="news-scraper@v1.4.1"
     )
 # -----------------------------------------
 
